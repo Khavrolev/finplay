ec22a8e91254d4d12a4745533a035f6b8c65a644
Sergey Khavrolev <sergey.khavrolev@introduct.tech>
2022-05-30 10:41:08 +0300

    not download images every rerender

--- finplay-frontend/src/components/Pages/UserPage/UserPage.tsx
+++ finplay-frontend/src/components/Pages/UserPage/UserPage.tsx
@@ -1,10 +1,11 @@
 import classNames from "classnames";
 import { observer } from "mobx-react-lite";
-import { FC, useCallback, useContext, useState } from "react";
+import { FC, useContext, useState } from "react";
 import Context from "../../../context";
 import { SORTING_TYPE } from "../../../utils/constants/filter";
 import { isFiltredGame } from "../../../utils/filter";
 import { IDiv } from "../../../utils/interfaces/components";
+import { IKey } from "../../../utils/interfaces/filter";
 import { IGame } from "../../../utils/interfaces/gameData";
 import Filter from "./Filter/Filter";
 import classes from "./UserPage.module.css";
@@ -20,13 +21,12 @@ const UserPage: FC<IDiv> = ({ divClass }) => {
 
   const [columnsCounter, setColumnsCounter] = useState(2);
 
-  const getFiltredGames = useCallback(() => {
-    return store.games.filter((game) =>
-      isFiltredGame(store.groups, game, store.filter),
-    );
-  }, [store.filter, store.games, store.groups]);
-
-  const filtredGames = getFiltredGames();
+  const filtredGames = store.games
+    .filter((game) => isFiltredGame(store.groups, game, store.filter))
+    .reduce((acc, item) => {
+      acc[item.id] = true;
+      return acc;
+    }, {} as IKey);
 
   const sortGames = (prev: IGame, cur: IGame) => {
     const sortingFunc = SORTING_TYPE[store.filter.sorting].func;
@@ -42,7 +42,7 @@ const UserPage: FC<IDiv> = ({ divClass }) => {
     <div className={classNames(divClass, classes.userpage)}>
       <Filter
         divClass={classes.userpage__filter}
-        countFiltredGames={filtredGames.length}
+        countFiltredGames={Object.keys(filtredGames).length}
         columnsCounter={columnsCounter}
         handleSliderChange={setColumnsCounter}
       />
@@ -50,23 +50,28 @@ const UserPage: FC<IDiv> = ({ divClass }) => {
         style={{ "--sliderValue": columnsCounter }}
         className={classNames(classes.userpage__games, classes.games)}
       >
-        {filtredGames.sort(sortGames).map((game) => (
-          <button
-            key={game.id}
-            className={classes.userpage__game}
-            onClick={() => console.log(game)}
-          >
-            <img
-              className={classes.userpage__img}
-              src={game.coverLarge}
-              onError={({ currentTarget }) => {
-                currentTarget.onerror = null;
-                currentTarget.src = game.cover;
-              }}
-              alt="cover"
-            />
-          </button>
-        ))}
+        {store.games
+          .slice()
+          .sort(sortGames)
+          .map((game) => (
+            <button
+              key={game.id}
+              className={classNames(classes.userpage__game, {
+                [classes.userpage__game_hidden]: !filtredGames[game.id],
+              })}
+              onClick={() => console.log(game)}
+            >
+              <img
+                className={classes.userpage__img}
+                src={game.coverLarge}
+                onError={({ currentTarget }) => {
+                  currentTarget.onerror = null;
+                  currentTarget.src = game.cover;
+                }}
+                alt="cover"
+              />
+            </button>
+          ))}
       </div>
     </div>
   );
