826b711cc0aec234e8d3127c258ecbd19996820d
Sergey Khavrolev <sergey.khavrolev@introduct.tech>
2022-05-31 22:01:35 +0300

    delete unused thing

--- finplay-frontend/src/utils/fetchData.ts
+++
@@ -1,8 +0,0 @@
-import { IGroup } from "./interfaces/gameData";
-
-const changeGroupInArray = (currentGroups: IGroup[], updatedGroup: IGroup) =>
-  currentGroups.map((item) =>
-    item.id === updatedGroup.id ? updatedGroup : item,
-  );
-
-export default changeGroupInArray;
